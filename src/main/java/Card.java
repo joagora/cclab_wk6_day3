0da76472ea1c2246d486857b83879ada4d7d90d0
mikethorpe <michael.a.thorpe@gmail.com>
2018-08-01 13:48:19 +0100

    Adding Card class with getters for suit and number + tests

--- src/main/java/Card.java
+++ src/main/java/Card.java
@@ -1,2 +1,19 @@
 public class Card {
+
+	private SuitType suit;
+	private NumberType number;
+
+	public Card(SuitType suit, NumberType number){
+		this.suit = suit;
+		this.number = number;
+	}
+
+	public SuitType getSuit(){
+		return this.suit;
+	}
+
+	public NumberType getNumber(){
+		return this.number;
+	}
+
 }
